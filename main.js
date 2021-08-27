fe3f94ce30943c6843099bfcf068ab2b991d1af9
zer0ken <a37592777@gmail.com>
2021-08-28 00:01:12 +0900

    save

--- main.js
+++ main.js
@@ -1,10 +1,29 @@
 $(window).ready(setLight)
 
+$(window).on({
+    mousemove: function (e) {
+        const [min, max] = [-16, 35]
+        const $room = $('#room')
+        $(':root').css('--y-rotation', (min + (max - min) * e.clientX / window.innerWidth) + 'deg')
+    },
+    mousewheel: function (e) {
+        changeLight(e.originalEvent.wheelDeltaY > 0 ? 1 : -1)
+    }
+})
+
 function setLight() {
     const hue = $(':root').css('--hue')
-    console.log(hue)
 
-    $('#large-window').css({ background: `linear-gradient(hsl(${hue}, 80%, 32%), hsl(${hue}, 60%, 40%))` })
-    $('#small-window').css({ background: `hsl(${hue}, 30%, 60%)` })
-    $('#gap').css({ background: `linear-gradient(-90deg, hsl(${hue}, 70%, 50%), hsl(${hue}, 30%, 30%))` })
+    $('#large-window').css({ background: `linear-gradient(170deg, hsl(${hue}, 70%, 22%), hsl(${hue}, 80%, 50%))` })
+    $('#small-window').css({ color: `hsl(${hue}, 75%, 50%)` })
+    $('#gap').css({ background: `linear-gradient(-90deg, hsl(${hue}, 75%, 50%), hsl(${hue}, 40%, 40%))` })
+    $('#ceil-shadow').css({ background: `linear-gradient(hsla(${hue}, 70%, 22%, .4), var(--ceil-color), var(--ceil-color))` })
+    $('#floor-shadow').css({ background: `linear-gradient(0deg, hsla(${hue}, 75%, 50%, .4), var(--floor-color), var(--floor-color))` })
+}
+
+function changeLight(delta) {
+    const $root = $(':root')
+    let hue = (parseInt($root.css('--hue')) + delta) % 360
+    $root.css('--hue', hue)
+    setLight()
 }
