6cc2aba138d35cf33b5418f494ecd353ba61bff8
zer0ken <a37592777@gmail.com>
2021-08-27 19:48:02 +0900

    hmm

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ function setLight() {
     const hue = $(':root').css('--hue')
     console.log(hue)
 
-    $('#large-window').css({ background: `linear-gradient(hsl(${hue}, 80%, 50%), hsl(${hue}, 60%, 40%))` })
-    $('#small-window').css({ background: `linear-gradient(hsl(${hue}, 50%, 40%), hsl(${hue}, 30%, 30%))` })
+    $('#large-window').css({ background: `linear-gradient(hsl(${hue}, 80%, 32%), hsl(${hue}, 60%, 40%))` })
+    $('#small-window').css({ background: `hsl(${hue}, 30%, 60%)` })
     $('#gap').css({ background: `linear-gradient(-90deg, hsl(${hue}, 70%, 50%), hsl(${hue}, 30%, 30%))` })
 }
